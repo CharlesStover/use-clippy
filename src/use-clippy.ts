1757cfccfa8d2ed145033ecd257c97bbb63ed56d
Charles Stover <github@charlesstover.com>
2019-03-20 12:16:40 -0700

    resolves #1

--- src/use-clippy.ts
+++ src/use-clippy.ts
@@ -1,9 +1,48 @@
-import useForceUpdate from 'use-force-update';
+import { useState, useEffect } from 'react';
+
+interface Clipboard {
+  readText(): Promise<string>;
+  writeText(text: string): Promise<void>;
+}
+
+interface ClipboardDataWindow extends Window {
+  clipboardData: DataTransfer | null;
+}
+
+type ClipboardEventListener =
+  EventListenerObject | null | ((event: ClipboardEvent) => void);
+
+interface ClipboardEventTarget extends EventTarget {
+  addEventListener(type: 'copy', eventListener: ClipboardEventListener): void;
+  addEventListener(type: 'cut', eventListener: ClipboardEventListener): void;
+  addEventListener(type: 'paste', eventListener: ClipboardEventListener): void;
+  removeEventListener(type: 'copy', eventListener: ClipboardEventListener):
+    void;
+  removeEventListener(type: 'cut', eventListener: ClipboardEventListener):
+    void;
+  removeEventListener(type: 'paste', eventListener: ClipboardEventListener):
+    void;
+}
+
+interface ClipboardNavigator extends Navigator {
+  clipboard: Clipboard & ClipboardEventTarget;
+}
 
 type ClipboardTuple = [
   string,
   (clipboard: string) => void,
 ];
+
+
+
+const IS_CLIPBOARD_API_ENABLED = (
+  typeof navigator === 'object' &&
+  typeof (navigator as ClipboardNavigator).clipboard === 'object'
+);
+
+const NOT_ALLOWED_ERROR = new Error('NotAllowed');
+
+
 
 const zeroStyles = (i: HTMLInputElement, ...properties: string[]): void => {
   for (const property of properties) {
@@ -41,10 +80,17 @@ const removeInput = (i: HTMLInputElement): void => {
   document.body.removeChild(i);
 };
 
-const read = () => {
+const read = (): string => {
   const i = createInput();
   i.focus();
-  document.execCommand('paste');
+  const success = document.execCommand('paste');
+
+  // If we don't have permission to read the clipboard,
+  //   cleanup and throw an error.
+  if (!success) {
+    removeInput(i);
+    throw NOT_ALLOWED_ERROR;
+  }
   const value = i.value;
   removeInput(i);
   return value;
@@ -54,22 +100,92 @@ const write = (text: string): void => {
   const i = createInput();
   i.setAttribute('value', text);
   i.select();
-  document.execCommand('copy');
+  const success = document.execCommand('copy');
   removeInput(i);
+  if (!success) {
+    throw NOT_ALLOWED_ERROR;
+  }
 };
 
 // TODO: async/await
 //   navigator.clipboard.readText() and navigator.clipboard.writeText()
-const useClippy = (rerender: boolean = true): ClipboardTuple => {
-  const forceUpdate = useForceUpdate();
+const useClippy = (): ClipboardTuple => {
+  const [ clipboard, setClipboard ] = useState('');
+
+  // If the user manually updates their clipboard,
+  //   re-render with the new value.
+  if (IS_CLIPBOARD_API_ENABLED) {
+    useEffect(() => {
+      const clipboardListener = ({ clipboardData }: ClipboardEvent) => {
+        const cd: DataTransfer | null =
+          clipboardData ||
+          (window as ClipboardDataWindow).clipboardData ||
+          null;
+        if (cd) {
+          const text = cd.getData('text/plain');
+          if (clipboard !== text) {
+            setClipboard(text);
+          }
+        }
+      };
+      const nav: ClipboardNavigator = navigator as ClipboardNavigator;
+      nav.clipboard.addEventListener('copy', clipboardListener);
+      nav.clipboard.addEventListener('cut', clipboardListener);
+      return () => {
+        nav.clipboard.removeEventListener('copy', clipboardListener);
+        nav.clipboard.removeEventListener('cut', clipboardListener);
+      };
+    }, [ clipboard, setClipboard ]);
+  }
+
+  // Try to read synchronously.
+  try {
+    const text = read();
+    if (clipboard !== text) {
+      setClipboard(text);
+    }
+  }
+
+  // If synchronous reading is disabled, try to read asynchronously.
+  catch (e) {
+    if (IS_CLIPBOARD_API_ENABLED) {
+      const nav: ClipboardNavigator = navigator as ClipboardNavigator;
+      nav.clipboard.readText()
+        .then(text => {
+          if (clipboard !== text) {
+            setClipboard(text);
+          }
+        })
+        .catch(() => {});
+    }
+  }
   return [
 
-    read(),
+    clipboard,
 
-    function clipboardWrite(text: string): void {
-      write(text);
-      if (rerender) {
-        forceUpdate();
+    function clippySetter(text: string): void {
+
+      /**
+       * Always update the clipboard and re-render,
+       *   even if the current value is the same.
+       * This accounts for when the user updates their clipboard
+       *   from outside the application.
+       * This also allows render animations to re-trigger,
+       *   e.g. "Copied successfully!"
+       */
+      try {
+        write(text);
+        setClipboard(text);
+      }
+      catch (e) {
+        if (IS_CLIPBOARD_API_ENABLED) {
+          const nav: ClipboardNavigator = navigator as ClipboardNavigator;
+          nav.clipboard.writeText(text)
+            .then(() => {
+              setClipboard(text);
+            })
+            .catch(() => {});
+        }
       }
     }
 
